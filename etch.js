ed6ef293557a673d9e341f689d13ca8f7344e227
James <jamesbaker425@gmail.com>
2020-06-24 14:14:41 -0400

    Update etch.js
    Cleaned up code

--- etch.js
+++ etch.js
@@ -1,15 +1,23 @@
+/*  
+    James Baker
+    6/23/20
+    jamesbaker425@gmail.com
+*/
+
+//Change grid square's color to black
 function draw(e) {
     e.target.style.backgroundColor = "black";
 }
 
+//Make all of the grid squares the original lightgray color
 function clear(){
-    console.log("Here");
     var gridSquares = document.getElementsByClassName("grid-item");
     for (var i = 0; i < gridSquares.length; i++) {
         gridSquares.item(i).style.backgroundColor = "lightgray";
-      } 
+    } 
 }
 
+//Populate grid-container with grid squares
 var container = document.getElementById("gc1");
 const size = Math.pow(16,2);
 
@@ -19,6 +27,7 @@ for (var i = 0; i < size; i++) {
     container.appendChild(node).className = "grid-item";
 }
 
+//Set up event listeners for drawing on hover and the clear button
 container.addEventListener("mouseover", draw);
 
 var clearButton = document.getElementById("clear");
